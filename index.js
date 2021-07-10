1ebe3169ea8a22797544cdf4d796304e6c639f3c
cjrequena828 <cjrequena827@gmail.com>
2021-07-10 11:49:13 -0400

    commit tree d5e57bfce379a8f50ae44ef63611d20e5e9f53d6

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const headQ = 42
+const mBlock = 264
+//let begBlock;
+//let desBlock;
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup >= 43)  
+    {return (pickup - headQ)}
+    if (pickup <= 42)
+    {return (headQ - pickup)}
+}
+function distanceFromHqInFeet(pickup, desBlock){
+    distanceFromHqInBlocks(pickup);
+    return ((pickup % desBlock)* mBlock)
+    
+    //distanceFromHqInBlocks * mBlock
+
+}
